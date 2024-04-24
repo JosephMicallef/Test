c457bd512bc86dfccf9ef8e70f77ee6de618c41c
Joseph Micallef <j.micallef@student.rug.nl>
2024-04-24 11:59:40 +0200

    Test By Joseph Micallef

---
+++ hello.c
@@ -0,0 +1,18 @@
+/* file   : hello.c 									 	   */
+/* author : Joseph Micallef (email: j.micallef@student.rug.nl) */
+/* date   : Sat Sep 16 2023 								   */
+/* version: 1.1 											   */
+
+/* Description:
+ * This program prints "Hello world!"
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+  printf("Balls\n");		/* Print Hello world! */
+  return 0;
+}
